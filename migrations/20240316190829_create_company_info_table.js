b270bffee21b0452709a7076cf8636b4f55f1712
Tsering Dhondup <tyddhondup88@gmail.com>
2024-03-16 15:29:38 -0400

    company_info migration file updated
    company_info seed data updated

--- migrations/20240316190829_create_company_info_table.js
+++ migrations/20240316190829_create_company_info_table.js
@@ -2,15 +2,32 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("company_info", (table) => {
+    table.increments("id").primary();
+    table
+      .integer("application_id")
+      .unsigned()
+      .references("applications.id")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    table.string("company_name").notNullable();
+    table.string("company_address").notNullable();
+    table.string("company_location").notNullable();
+    table.string("company_about").notNullable;
+    table.string("reference_name").notNullable();
+    table.string("reference_contact").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-    return knex.schema.dropTable("profiles");
-
+exports.down = function (knex) {
+  return knex.schema.dropTable("company_info");
 };
