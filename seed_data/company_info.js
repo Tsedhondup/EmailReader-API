697cf784221f25164017d6819d2cbe0c81640282
Tsering Dhondup <tyddhondup88@gmail.com>
2024-03-17 22:39:46 -0400

    company_about data were shortened and replaced with link to sspecific website

--- seed_data/company_info.js
+++ seed_data/company_info.js
@@ -5,9 +5,7 @@ module.exports = [
     company_name: "BrainStation",
     company_address: "486 Front St W, Toronto, ON M5V 0V2",
     company_location: "Toronto",
-    company_about: `BrainStation is the global leader in digital skills training, empowering businesses and brands to succeed in the digital age.
-    Established in 2012, BrainStation has worked with over 500 instructors from the most innovative companies, developing cutting-edge, 
-    real-world digital education that has empowered more than 100,000 professionals and some of the largest corporations in the world.`,
+    company_about: `https://www.jobsage.com/companies/about/brainstation`,
     reference_name: "Brian",
     reference_contact: "brian12@gmail.com",
   },
