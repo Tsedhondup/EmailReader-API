e326fd11fea799c4b09648f3523f6c50aa7cc51e
Tsering Dhondup <tyddhondup88@gmail.com>
2024-03-23 12:38:30 -0400

    sent_mail seed data updated

--- seed_data/sent_emails.js
+++ seed_data/sent_emails.js
@@ -5,6 +5,10 @@ module.exports = [
     email_date: "234234234",
     message_id: "12345678",
     subject: "follow up",
+    from: "tyddhondup88@gmail.com",
     to: "job.application_tracker@gmail.com",
+    reply_to: "job.application_tracker@gmail.com",
+    mail_header: "<style></style>",
+    mail_body: "<p>thank you for having me<p>",
   },
 ];
