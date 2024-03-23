2adedf22ec00d9a4b5d7a62fce23ba8c693031f3
Tsering Dhondup <tyddhondup88@gmail.com>
2024-03-23 12:48:18 -0400

    sent_mail migration file updated

--- migrations/20240323163002_send_email.js
+++ migrations/20240323163002_send_email.js
@@ -2,14 +2,33 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("sent_mail", (table) => {
+    table.increments("id").primary();
+    table
+      .integer("application_id")
+      .unsigned()
+      .references("applications.id")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    table.string("from").notNullable();
+    table.string("to").notNullable();
+    table.string("reply_to").notNullable();
+    table.string("subject").notNullable;
+    table.string("email_date").notNullable();
+    table.string("email_html_style").notNullable();
+    table.string("email_html_style").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("sent_mail");
 };
