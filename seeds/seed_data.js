ea328e86aa95898b1c12b7361aa6cc816979103e
Tsering Dhondup <tyddhondup88@gmail.com>
2024-03-23 12:52:16 -0400

    sent_email migration file name chaged
    seeding file updated

--- seeds/seed_data.js
+++ seeds/seed_data.js
@@ -9,6 +9,7 @@ const applications = require("../seed_data/applications");
 const emails = require("../seed_data/emails");
 const interviews = require("../seed_data/interviews");
 const company_info = require("../seed_data/company_info");
+const sent_emails = require("../seed_data/sent_emails");
 
 // exports.seed = async function (knex) {
 //   // Deletes ALL existing entries
@@ -28,15 +29,19 @@ exports.seed = function (knex) {
         .del();
     })
     .then(() => {
-      return knex("company_info") // delete emails table
+      return knex("company_info") // delete company_info table
         .del();
     })
     .then(() => {
-      return knex("applications") // delete emails table
+      return knex("sent_email") // delete sent_mail table
         .del();
     })
     .then(() => {
-      return knex("profiles") // delete emails table
+      return knex("applications") // delete application table
+        .del();
+    })
+    .then(() => {
+      return knex("profiles") // delete profile table
         .del();
     })
     .then(function () {
@@ -44,6 +49,9 @@ exports.seed = function (knex) {
     })
     .then(() => {
       return knex("applications").insert(applications);
+    })
+    .then(() => {
+      return knex("sent_email").insert(sent_emails);
     })
     .then(() => {
       return knex("company_info").insert(company_info);
