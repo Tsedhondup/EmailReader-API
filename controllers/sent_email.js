b8134fd62660b88d1d31ca7323e9b92ae1ac3b4b
Tsering Dhondup <tyddhondup88@gmail.com>
2024-03-24 16:05:54 -0400

    getting all sent_emails completed

--- controllers/sent_email.js
+++ controllers/sent_email.js
@@ -14,7 +14,6 @@ const sendEmail = async (req, res) => {
       port: 465,
       secure: true, // Use `true` for port 465, `false` for all other ports
       auth: {
-    
         user: parsedData.email,
         pass: parsedData.password,
       },
@@ -66,101 +65,13 @@ const sendEmail = async (req, res) => {
   });
 };
 const getSentEmails = async (req, res) => {
-  await fs.readFile("./session/session.json", (err, data) => {
-    const parsedData = JSON.parse(data); // config IMAP
-    const imap = new Imap({
-      user: process.env.MY_EMAIL,
-      password: process.env.MY_PS, // my app
-      // user: parsedData.email,
-      // password: parsedData.password, // my app
-      host: "imap.gmail.com",
-      port: 993,
-      authTimeout: 10000,
-      tls: true,
-      tlsOptions: { rejectUnauthorized: false },
+  knex("sent_emails")
+    .then((sent_emails) => {
+      res.status(200).json({ sent_emails });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Sent emails not found" });
     });
-
-    // when IMAP connection is ready
-    imap.once("ready", () => {
-      console.log("connection successful");
-      // open email-box
-      imap.openBox("SENT", true, (err, box) => {
-        // error
-        if (err) throw err;
-
-        // search criteria
-        imap.search(
-          [
-            ["SINCE", "Jan 1, 2024"],
-            ["HEADER", "TO", "tdhondup2022@gmail.com"],
-          ],
-          function (err, results) {
-            if (err) throw err;
-
-            // STORE MESSAGE BODY IN VARIABLE 'f'
-            let f;
-            // IF NO EMAIL IS RECIEVE THE ABORT THE CONNECTION AND EXIT FUNCTIONS
-            try {
-              f = imap.fetch(results, { bodies: "" });
-            } catch (err) {
-              if (err) {
-                console.log(err);
-                imap.end();
-                return;
-              }
-            }
-            // once app starts recieving email
-            f.on("message", function (msg, seqno) {
-              // once app have the message body
-              msg.on("body", async function (stream, info) {
-                // parse messsage data using Nodemailer simple parser
-                let parsed = await simpleParser(stream);
-                console.log(parsed);
-
-                // Creating email object
-                // const emailObject = {
-                //   application_id: applicationId,
-                //   email_date: `${parsed.date}`,
-                //   message_id: parsed.headers.get("message-id"),
-                //   subject: parsed.headers.get("subject")
-                //     ? parsed.headers.get("subject")
-                //     : "No Subject",
-                //   from: parsed.from.value[0].address,
-                // };
-
-                // INVOKE addEmails() to add email to database
-                // addEmails(emailObject);
-              });
-            });
-
-            // if there is error fetching message/email
-            f.once("error", function (err) {
-              console.log("Fetch error: " + err);
-            });
-
-            // once the fetching message/email is completed
-            f.once("end", function () {
-              console.log("Done fetching all messages!");
-              imap.end();
-            });
-          }
-        );
-      });
-    });
-
-    // when IMAP connection failed
-    imap.once("error", function (err) {
-      console.log(err);
-    });
-
-    // when IMAP connection was ended
-    imap.once("end", function () {
-      console.log("Connection ended");
-    });
-
-    // Establishing IMAP connection
-    imap.connect();
-  });
 };
 module.exports = {
   sendEmail,
--- routes/send_email_routes.js
+++ routes/send_email_routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const sendEmail = require("../controllers/sent_email");
 
 router.route("/sendEmail").post(sendEmail.sendEmail);
+router.route("/getSendEmails").get(sendEmail.getSentEmails);
 
 module.exports = router;
