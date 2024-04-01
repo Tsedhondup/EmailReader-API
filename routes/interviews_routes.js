eac92c36095d3e3acbd9f0dbd0fcd95469c8ef1e
Tsering Dhondup <tyddhondup88@gmail.com>
2024-04-01 16:16:40 -0400

    updated interviews route for updateing interviews

--- routes/interviews_routes.js
+++ routes/interviews_routes.js
@@ -4,6 +4,8 @@ const interviewController = require("../controllers/interviews_controller");
 router.route("/allInterviews/:id").get(interviewController.getInterviews);
 
 router.route("/addInterviews").post(interviewController.addInterviews);
-router.route("/updateInterview/:id").patch(interviewController.addInterviews);
+router
+  .route("/updateInterview/:id")
+  .patch(interviewController.updateInterviews);
 
 module.exports = router;
