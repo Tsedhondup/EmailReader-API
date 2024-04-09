fbfff3a395485a678ea3e977e4377ac169a2f872
Tsering Dhondup <tyddhondup88@gmail.com>
2024-04-08 23:40:34 -0400

    update all route for interview commeted ATM

--- routes/interviews_routes.js
+++ routes/interviews_routes.js
@@ -5,6 +5,6 @@ router.route("/allInterviews/:id").get(interviewController.getInterviews);
 
 router.route("/addInterviews").post(interviewController.addInterviews);
 router.route("/updateInterview/:id").patch(interviewController.updateSome);
-router.route("/updateInterview/:id").patch(interviewController.updateAll);
+// router.route("/updateInterview/:id").patch(interviewController.updateAll);
 
 module.exports = router;
